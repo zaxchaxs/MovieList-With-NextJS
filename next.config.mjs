f8dc95a6a9ff10a20822f391446d9d09a671de2a
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-03-09 10:24:01 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        API_KEY: "9e3138912fc7ba186a0cdb9f5e472f81",
+        API_BASE_ENDPOINT: "https://api.themoviedb.org/3/",
+        API_READ_ACCESS_TOKEN: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTMxMzg5MTJmYzdiYTE4NmEwY2RiOWY1ZTQ3MmY4MSIsInN1YiI6IjY1ZWFlYmFhOTRkOGE4MDE2MThmMjBkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zJ4xN9AnwPMeyRQcO8JjQgagZUvd3BFwLpuNUtu6ny4",
+        
+    }
+};
 
 export default nextConfig;
