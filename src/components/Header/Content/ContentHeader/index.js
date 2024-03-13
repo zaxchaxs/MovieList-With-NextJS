218f9999b55e1fdaf2cbe8da1d9536e3196f5951
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-03-13 16:28:15 +0700

    update detail compo

--- src/components/Header/Content/ContentHeader/index.js
+++ src/components/Header/Content/ContentHeader/index.js
@@ -15,6 +15,16 @@ const roboto = Roboto({
 export default function ContentHeader () {
     const [isMovieClicked, setMovieClicked] = useState(false);
     const [isSeriesClicked, setSeriesClicked] = useState(false);
+    const routeMovies = {
+        popular : "/movies",
+        topRated : "#",
+        upComing : "#"
+    };
+    const routeSeries = {
+        popular : "/series",
+        topRated : "#",
+        upComing : "#"
+    };
 
     return(
     <div className={`flex justify-center items-center gap-5 font-bold text-white`}>
@@ -22,28 +32,28 @@ export default function ContentHeader () {
         <ul className="flex gap-7 text-base">
             <li className="group">
                 <input type="button" className="cursor-pointer" value={"Movies"} onClick={() => setMovieClicked(!isMovieClicked)} />
-                <ListContent isMovieClicked={isMovieClicked} />
+                <ListContent isMovieClicked={isMovieClicked} route={routeMovies} />
             </li>
             <li className="group">
                 <input type="button" className="cursor-pointer" value={"TV Series"} onClick={() => setSeriesClicked(!isSeriesClicked)} />
-                <ListContent isSeriesClicked={isSeriesClicked} />
+                <ListContent isSeriesClicked={isSeriesClicked} route={routeSeries} />
             </li>
         </ul>
     </div>
     )
 }
 
-function ListContent({ isMovieClicked, isSeriesClicked }) {
+function ListContent({ isMovieClicked, isSeriesClicked, route }) {
     return(
         <ul className={"absolute rounded-md w-fit h-auto p-4 bg-red-900 font-bold text-white shadow-md " + (isMovieClicked||isSeriesClicked ? "" : "hidden")}>
             <li className="transform transition-transform duration-300 hover:translate-x-2 ease-in-out">
-                <Link href={"#"}>Popular</Link>
+                <Link href={route.popular}>Popular</Link>
             </li>
             <li className="transform transition-transform duration-300 hover:translate-x-2 ease-in-out">
-                <Link href={"#"}>Top Rated</Link>
+                <Link href={route.topRated}>Top Rated</Link>
             </li>
             <li className="transform transition-transform duration-300 hover:translate-x-2 ease-in-out">
-                <Link href={"#"}>Now Playing</Link>
+                <Link href={route.upComing}>Up Coming</Link>
             </li>
         </ul>        
     )
